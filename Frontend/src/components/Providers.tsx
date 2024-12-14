04ef007cee31ccf04c92e39944fdf06cadc0e194
gadisamenu <amegadbay@gmail.com>
2024-12-14 22:38:51 +0300

    clean frontend files

--- Frontend/src/components/Providers.tsx
+++ Frontend/src/components/Providers.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { persistor, store } from "../store";
 import Layout from "./common/Layout";
-import { ThemeProvider } from "./theme-provider";
 import { PersistGate } from "redux-persist/integration/react";
 type Props = {
   children: ReactNode;
@@ -14,14 +13,7 @@ const Providers = ({ children }: Props) => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Layout>{children}</Layout>
-        </ThemeProvider>
+        <Layout>{children}</Layout>
       </PersistGate>
     </Provider>
   );
